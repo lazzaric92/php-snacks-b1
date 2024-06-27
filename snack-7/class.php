43ebc9f405a958ad957efd0569454c82609929d8
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-27 12:35:59 +0200

    added snack-7 (wip)

---
+++ snack-7/class.php
@@ -0,0 +1,34 @@
+<?php 
+$class = [
+    [
+        "name" => "Bruco",
+        "lastName" => "Gianluco",
+        "marks" => [
+            rand(0,10), rand(0,10), rand(0,10), rand(0,10), rand(0,10),
+        ]
+    ],
+    [
+        "name" => "Nonno",
+        "lastName" => "Laser",
+        "marks" => [
+            rand(0,10), rand(0,10), rand(0,10), rand(0,10), rand(0,10),
+        ]
+    ],
+    [
+        "name" => "Rogiorzo",
+        "lastName" => "Pastifralli",
+        "marks" => [
+            rand(0,10), rand(0,10), rand(0,10), rand(0,10), rand(0,10),
+        ]
+    ],
+    [
+        "name" => "Sghe",
+        "lastName" => "Flemboo",
+        "marks" => [
+            rand(0,10), rand(0,10), rand(0,10), rand(0,10), rand(0,10),
+        ]
+    ],
+];
+
+
+?>
---
+++ snack-7/index.php
@@ -0,0 +1,13 @@
+<!-- // todo: Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Questo array dovra' essere inserito in un file separato, da importare. Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php 
+include_once __DIR__ . "/class.php";
+
+?>
+
+<h1>Class #</h1>
+<ul>
+    <?php foreach ($class as $student) { ?>
+        <h2> <?php echo $student["name"] . " " . $student["lastName"] ?> </h2>
+    <?php } ?>
+</ul>
